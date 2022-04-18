8494ad2a925f754d85790f5eada1731124cfd751
SuperAxFax <2787086427@qq.com>
2022-04-18 22:59:13 +0800

    第一个springboot程序

---
+++ springboot-01-helloworld/src/main/java/com/kuang/Springboot01HelloworldApplication.java
@@ -0,0 +1,14 @@
+package com.kuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//标注这个类是一个springboot应用：启动类下的所有资源被导入
+@SpringBootApplication
+public class Springboot01HelloworldApplication {
+
+    public static void main(String[] args) {
+        //将springboot应用启动
+        SpringApplication.run(Springboot01HelloworldApplication.class, args);
+    }
+
+}
